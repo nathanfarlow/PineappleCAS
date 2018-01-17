187d4dbb302900c51a9d0d3b2ac010fac01aef67
Nathan Farlow <natethegreatest11@gmail.com>
2018-01-16 20:30:28 -0600

    Update calculator functionality

---
+++ src/calculator/ftoa.c
@@ -0,0 +1,144 @@
+/**
+ *  stm32tpl --  STM32 C++ Template Peripheral Library
+ *
+ *  Copyright (c) 2009-2014 Anton B. Gusev aka AHTOXA
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in
+ *  all copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ *
+ *
+ *  file         : ftoa.c
+ *  description  : convert double to string
+ *
+ */
+
+/*
+	This file will be removed later, but it is being used now for debugging while there
+	is no good support to print doubles on the CE
+*/
+
+#ifndef COMPILE_PC
+
+#define MAX_PRECISION	(10)
+static const double rounders[MAX_PRECISION + 1] =
+{
+	0.5,				// 0
+	0.05,				// 1
+	0.005,				// 2
+	0.0005,				// 3
+	0.00005,			// 4
+	0.000005,			// 5
+	0.0000005,			// 6
+	0.00000005,			// 7
+	0.000000005,		// 8
+	0.0000000005,		// 9
+	0.00000000005		// 10
+};
+
+char * ftoa(double f, char * buf, int precision)
+{
+	char * ptr = buf;
+	char * p = ptr;
+	char * p1;
+	char c;
+	long intPart;
+
+	// check precision bounds
+	if (precision > MAX_PRECISION)
+		precision = MAX_PRECISION;
+
+	// sign stuff
+	if (f < 0)
+	{
+		f = -f;
+		*ptr++ = '-';
+	}
+
+	if (precision < 0)  // negative precision == automatic precision guess
+	{
+		if (f < 1.0) precision = 6;
+		else if (f < 10.0) precision = 5;
+		else if (f < 100.0) precision = 4;
+		else if (f < 1000.0) precision = 3;
+		else if (f < 10000.0) precision = 2;
+		else if (f < 100000.0) precision = 1;
+		else precision = 0;
+	}
+
+	// round value according the precision
+	if (precision)
+		f += rounders[precision];
+
+	// integer part...
+	intPart = f;
+	f -= intPart;
+
+	if (!intPart)
+		*ptr++ = '0';
+	else
+	{
+		// save start pointer
+		p = ptr;
+
+		// convert (reverse order)
+		while (intPart)
+		{
+			*p++ = '0' + intPart % 10;
+			intPart /= 10;
+		}
+
+		// save end pos
+		p1 = p;
+
+		// reverse result
+		while (p > ptr)
+		{
+			c = *--p;
+			*p = *ptr;
+			*ptr++ = c;
+		}
+
+		// restore end pos
+		ptr = p1;
+	}
+
+	// decimal part
+	if (precision)
+	{
+		// place decimal point
+		*ptr++ = '.';
+
+		// convert
+		while (precision--)
+		{
+			f *= 10.0;
+			c = f;
+			*ptr++ = '0' + c;
+			f -= c;
+		}
+	}
+
+	// terminating zero
+	*ptr = 0;
+
+	return buf;
+}
+
+#else
+typedef int make_iso_compilers_happy;
+#endif
--- src/calculator/main.c
+++ src/calculator/main.c
@@ -4,6 +4,9 @@
 #include <fileioc.h>
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
 
 #include "../dbg.h"
 
@@ -13,10 +16,7 @@
 #include "../cas/cas.h"
 #include "../cas/mapping.h"
 
-void print_text(int8_t xpos, int8_t ypos, const char *text) {
-    os_SetCursorPos(ypos, xpos);
-    os_PutStrFull(text);
-}
+extern char *ftoa(double f, char *buf, int precision);
 
 void main() {
     ti_var_t y1;
@@ -43,7 +43,9 @@ void main() {
         if(err != E_SUCCESS) {
             LOG(("Couldn't parse equation, reason: %s", error_text[err]));
         } else {
-            double value1, value2;
+            double value;
+            char buffer[50];
+
             ast_t *x = ast_MakeNumber(num_CreateDecimal("2.53"));
 
             dbg_print_tree(e, 4);
@@ -51,22 +53,27 @@ void main() {
             mapping_Init();
             mapping_Set('X', x);
 
-            value1 = approximate(e, &err);
+            value = approximate(e, &err);
+            if(err == E_SUCCESS) {
+                ftoa(value, buffer, 10);
+                DBG(("Eval: %s\n\n", buffer));
+            } else {
+                DBG(("Unable to evaluate, reason: %s\n\n", error_text[err]));
+            }
 
             simplify(e);
 
             DBG(("Simplified:\n"));
             dbg_print_tree(e, 4);
 
-            value2 = approximate(e, &err);
+            value = approximate(e, &err);
             if(err == E_SUCCESS) {
-                if(value1 == value2) {
-                    DBG(("approximate() returned the same result."));
-                } else {
-                    DBG(("approximate() DID NOT return the same result."));
-                }
-            } else
+                ftoa(value, buffer, 10);
+                DBG(("Eval: %s\n", buffer));
+            } else {
                 DBG(("Unable to evaluate, reason: %s\n\n", error_text[err]));
+            }
+
         }
 
         ast_Cleanup(e);
