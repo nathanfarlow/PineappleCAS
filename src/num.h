7af24c2de14536835f8bd7a32a66ed2b05522202
Nathan Farlow <natethegreatest11@gmail.com>
2018-01-18 23:45:59 -0600

    Remove message

--- src/num.h
+++ src/num.h
@@ -7,18 +7,6 @@
 #include "imath/imath.h"
 #include "imath/imrat.h"
 
-/*
-A note:
-I wasn't originally planning to have the num_t struct always be treated using pointers,
-but there is a bug with the ez80 compiler that disallows nested functions that return
-non pointers.
-What I mean is, I wanted to write num_t num_Create() instead of num_t *num_Create().
-Is there a speed difference I wonder? Is passing it by pointer everywhere faster anyway?
-
-Maybe someday we will write a bignum library out of this num_t struct to aid in the
-simplification process. I don't know if that's really practical on the calculator, though.
-*/
-
 typedef struct _Num {
     bool is_decimal;
     union {
